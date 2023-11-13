58b96176e2ff04392946e54f71baaa2069e75a44
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-11-13 02:25:15 -0500

    express server created, dependencies installed, and rm pages api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,36 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+const port = 3001;
 
-app.listen(5000, () =>{
-     console.log('Express Server listening on port: 5000')
+app.use(cors())
+app.use(express.json())
+
+app.get('api/v1/demo', (req, res, next) => {
+     try {
+          const artists = [
+               {id: 1, 'name': 'Hunxho'},
+               {id: 2, 'name': 'Trippie Redd'},
+          ];
+
+          return res.status(200).json({artists})
+     } catch (err) {
+          console.error(err)
+     }
+})
+app.get('api/v1/spotify', (req, res) => {
+     try {
+          const artists = [
+               {id: 1, 'name': 'Hunxho'},
+               {id: 2, 'name': 'Trippie Redd'},
+          ];
+
+          return res.status(200).json({artists})
+     } catch (err) {
+          console.error(err)
+     }
+});
+
+app.listen(port, () =>{
+   console.log(`Express Server listening on port: ${port}`)
 })
